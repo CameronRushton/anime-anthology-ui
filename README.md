3cd1f0643ee9db41c6696f4ce9d59f4a8d4f47f6
Cameron Rushton <32455969+CameronRushton@users.noreply.github.com>
2020-04-25 02:21:03 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+﻿# Website initializer/template in aurelia
---
+++ src/app.js
@@ -0,0 +1,32 @@
+import { inject, PLATFORM } from "aurelia-framework";
+import { Router } from "aurelia-router"
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import {activationStrategy} from 'aurelia-router';
+
+@inject(Router)
+export class App {
+
+	constructor(router) {
+		this.router = router;
+	}
+
+	navigateToRoute() {
+		this.router.navigateToRoute('/')
+	}
+
+	configureRouter(config, router) {
+		config.title = "Aurelia Initializr";
+		config.options.pushState = true;
+		config.options.root = "/";
+		config.map([
+			{
+				route: '/',
+				name: 'home',
+				moduleId: PLATFORM.moduleName('pages/home/home'),
+				title: "Home",
+			}
+		]);
+	}
+
+}
+
