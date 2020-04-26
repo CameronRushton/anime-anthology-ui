8f00f8b0e4b8bb1ed9055ed7f256228581be3f7e
cameronrushton <cameron.rushton@rogers.com>
2020-04-25 21:52:19 -0400

    added anilist api example

---
+++ src/managers/anilist-api-manager.js
@@ -0,0 +1,99 @@
+
+export class AnilistAPIManager {
+    constructor() {
+    }
+
+    queryAnilist(query, variables) {
+        let url = 'https://graphql.anilist.co'
+        let options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            body: JSON.stringify({
+                query: query,
+                variables: variables
+            })
+        };
+        
+        return fetch(url, options).then(this.json).catch(this.handleError);
+    }
+
+    json(response) {
+        return response.json().then(json => {
+            return response.ok ? json : Promise.reject(json);
+        });
+    }
+
+    handleError(response) {
+        if (response.status >= 400 && response.status <= 599) {
+			throw response;
+		}
+		return response;
+    }
+
+    // TODO: How should queries be written? Should we make a builder? Put queries in a file and import the variables in a manager?
+    getHighestRatedAllTime() {
+        let query = `
+            {
+                Highest_Rated_All_time:Page(page:1,perPage:6) {
+                media(sort:SCORE_DESC, type:ANIME, isAdult:false) {
+                    id 
+                    title {
+                    romaji
+                    english
+                    native
+                    userPreferred
+                    }
+                    coverImage {
+                    extraLarge
+                    large
+                    medium
+                    color
+                    }
+                    startDate{
+                        year 
+                        month 
+                        day
+                    }
+                    endDate{
+                        year 
+                        month 
+                        day
+                    }
+                    bannerImage 
+                    season 
+                    description 
+                    type 
+                    format 
+                    status 
+                    genres 
+                    isAdult 
+                    averageScore 
+                    popularity 
+                    mediaListEntry{
+                        id 
+                        status
+                    }
+                    nextAiringEpisode{
+                        airingAt 
+                        timeUntilAiring 
+                        episode
+                    }
+                    studios(isMain:true){
+                        edges{
+                            isMain 
+                            node
+                            {
+                                id 
+                                name
+                            }
+                        }
+                    }
+                }
+            }
+        }`
+        return this.queryAnilist(query);
+    }
+}
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,16 +1,17 @@
 import { inject } from 'aurelia-framework';
 import { Router } from "aurelia-router"
+import { AnilistAPIManager } from 'managers/anilist-api-manager';
 
-@inject(Router)
+@inject(Router, AnilistAPIManager)
 export class Home {
     
-    constructor(router) {
+    constructor(router, anilistAPIManager) {
         this.router = router;
-
+        this.anilistAPI = anilistAPIManager;
     }
 
     attached() {
-        
+        console.log(this.anilistAPI.getHighestRatedAllTime());
     }
 
 }
